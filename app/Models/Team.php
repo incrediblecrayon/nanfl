8519457323ffb66bd0206c024d36759b43209a99
Joel Frederick <joel@nowvoid.com>
2019-01-18 13:22:26 -0800

    Adding basic relations in models

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'city'
+    ];
+
+    //Relations
+    public function players()
+    {
+        return $this->hasMany('App\Models\Player');
+    }
+
+    //Adjusting Attributes
+    public function getNameAttribute($value)
+    {
+        return title_case($value);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
+
+    public function getCityAttribute($value)
+    {
+        return title_case($value);
+    }
+
+    public function setCityAttribute($value)
+    {
+        $this->attributes['city'] = strtolower($value);
+    }
 }
