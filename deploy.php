dbf7e78894535667163a74d40b6a12aeb6b94582
Joel Frederick <joel@nowvoid.com>
2019-01-21 03:25:44 -0800

    Addind small deploy changes

--- deploy.php
+++ deploy.php
@@ -51,11 +51,13 @@ task('bundlejs', function () {
 //    }else{
 //        runLocally("npm run production");
 //    }
-//
-//    //Upload Bundled JS Files
+
+    runLocally("npm run production");
+
+    //Upload Bundled JS Files
 //    upload('public/js/*', '{{release_path}}/public/js/');
-//    upload('public/css/*', '{{release_path}}/public/css/');
-//    upload('public/img/*', '{{release_path}}/public/img/');
+    upload('public/css/*', '{{release_path}}/public/css/');
+    upload('public/img/*', '{{release_path}}/public/img/');
 //    upload('public/images/*', '{{release_path}}/public/images/');
 //    upload('public/vendors/*', '{{release_path}}/public/vendors/');
 //    upload('public/fonts/*', '{{release_path}}/public/fonts/');
